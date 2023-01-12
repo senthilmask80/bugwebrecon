5ff2e37f7278140efc53e8b7ba6bd3b4b7128003
senthilmask80 <53327340+senthilmask80@users.noreply.github.com>
2023-01-12 08:50:06 +0530

    Update buginstall.sh

--- buginstall.sh
+++ buginstall.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sudo apt-get update;
+sudo apt-get upgrade -y;
+sudo apt-get install curl wget git git-all;
 sudo apt-get install python3;
 sudo apt-get install python3-pip;
 sudo apt-get install ruby;
