659acdba6a8af7e5874f61fcacfb077c3f165dec
senthilmask80 <53327340+senthilmask80@users.noreply.github.com>
2023-01-17 01:18:23 +0530

    Update buginstall.sh

--- buginstall.sh
+++ buginstall.sh
@@ -1,7 +1,35 @@
 #!/bin/sh
 
-sudo apt-get update;
-sudo apt-get upgrade -y;
+WORKING_DIR="$(cd "$(dirname "$0")" ; pwd -P)"
+TOOLS_PATH="$WORKING_DIR/tools"
+WORDLIST_PATH="$WORKING_DIR/wordlists"
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+BLUE="\033[1;36m"
+RESET="\033[0m"
+
+installBanner(){
+    name=$1
+    echo -e "${RED}[+] Installing $name...${RESET}"
+}
+
+update(){
+    echo -e "${GREEN}\n--==[ Setting things up ]==--${RESET}"
+    echo -e "${RED}[+] Updating...${RESET}"
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+    sudo apt-get autoremove -y
+    sudo apt clean
+}
+
+createDir(){
+    echo -e "${RED}[+] Creating directories...${RESET}"
+    mkdir -p $TOOLS_PATH $WORDLIST_PATH
+    echo -e "${BLUE}[*] $TOOLS_PATH${RESET}"
+    echo -e "${BLUE}[*] $WORDLIST_PATH\n${RESET}"
+}
+
+
 sudo apt-get build-essential apt-transport-https software-properties-common gcc;
 sudo apt-get install curl wget git git-all rename screen certbot gunzip zip unzip 7zip tar;
 sudo apt-get install openssl openssh-server openssh-client;
@@ -14,14 +42,14 @@ sudo apt-get install npm;
 sudo apt-get install sqlite sqlite3 sqlite3-tools sqlitebrowser;
 sudo apt-get install dnsmasq dnsmasq-base dnsproxy dnsutils dnsrecon dns2tcp dnscache;
 sudo apt-get install -y make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev;
-sudo apt-get install llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libpcap-dev;
+sudo apt-get install llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libpcap-dev xsltproc libldns-dev;
 sudo apt-get install dnsutils libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ibssl-dev libffi-dev;
 sudo apt-get install lzma liblzma-dev libbz2-dev readline gnureadline;
 sudo apt-get install python-is-python3 python3-pip python3-venv python3-virtualenv;
 sudo apt-get install python3-dev python3-minimal python3-full;
 sudo apt-get install python2.7-dev python2.7-minimal python2.7;
 sudo apt-get install python3-setuptools python3-requests python3-wheel python3-wheel-whl;
-sudo apt-get install pythonpy python3-poetry python3-poetry-core;
+sudo apt-get install pythonpy python3-poetry python3-poetry-core pipenv;
 sudo apt-get install python3-tk python3-whois python3-dns python3-dnslib python3-dnspython python3-dnsq;
 sudo apt-get install python3-httpx python3-httptools python3-parser python3-httpbin python3-httpcore;
 sudo apt-get install python3-httplib2 python3-httpretty python3-args python3-argparse-manpage;
@@ -30,7 +58,7 @@ sudo apt-get install python3-aiohttp-proxy python3-aiohttp-security python3-aioh
 sudo apt-get install nmap tor torbrowser-launcher tor-geoipdb torsocks iptables iptables-persistent;
 sudo apt-get install onionshare onioncircuits onionshare unbound proxychains proxychains4 proxycheck;
 sudo apt-get install chromium-browser chromium-chromedriver;
-sudo apt-get install 0install 0install-core
+sudo apt-get install 0install 0install-core;
 pip install pipreqs;
 pip install pypi-install;
 pip install feedparser;
@@ -41,6 +69,7 @@ pip install pyopenssl;
 pip3 install corscanner;
 pip install "urllib3[secure]"
 pip install mmh3;
+pip install aort;
 
 mkdir ~/.gf
 mkdir ~/Tools;
@@ -88,6 +117,9 @@ cp $dir/massdns/bin/massdns /usr/bin/;
 cd ~/.gf; wget https://raw.githubusercontent.com/devanshbatham/ParamSpider/master/gf_profiles/potential.json;
 cd ~/go/bin; wget https://raw.githubusercontent.com/Ice3man543/SubOver/master/providers.json;
 cd ~/Tools; wget https://gist.githubusercontent.com/KathanP19/d2cda2f99c0b60d64b76ee6039b37e47/raw/eb105a4de06502b2732df9d682c61189c3703685/jsvar.sh;
+
+echo "PATH=${PATH}:~/.local/bin" >> ~/.bashrc
+
 
 
 ##########################Sublister##########################
@@ -259,3 +291,181 @@ wget https://github.com/SilverPoision/San/blob/master/word.txt
 cd ..
 echo "Vhost Done"
 echo -e \n\n "${RED}Once again don't forget to set the api keys for subfinder from here https://github.com/subfinder/subfinder#post-installation-instructions and also setting the AWS CLI creds. ${NC}"
+
+setupTools(){
+    installBanner "setup tools"
+    INSTALL_PKGS="git python python-pip python3 python3-pip libldns-dev gcc g++ make libpcap-dev xsltproc curl"
+    for i in $INSTALL_PKGS; do
+        sudo apt-get install -y $i
+    done
+
+    if [ "ubuntu" == "$(cat /etc/os-release | grep ^ID= | cut -d '=' -f2)" ]; then
+        sudo add-apt-repository ppa:canonical-chromium-builds/stage -y
+        sudo apt update -y
+        sudo apt install -y chromium-browser
+    else
+        sudo apt install -y chromium
+    fi
+}
+
+
+subEnumTools(){
+    echo -e "${GREEN}\n--==[ Installing subdomain enum tools ]==--${RESET}"
+    installBanner "Amass"
+    if [ -e ~/go/bin/amass ]; then
+        echo -e "${BLUE}[!] Amass already exists...\n${RESET}"
+    else 
+        go get -u github.com/OWASP/Amass/...
+    fi
+    
+    installBanner "subfinder"
+    if [ -e ~/go/bin/subfinder ]; then
+        echo -e "${BLUE}[!] Subfinder already exists...\n${RESET}"
+    else 
+        go get -u github.com/subfinder/subfinder
+        echo -e "${RED}[+] Setting up API keys for subfinder...${RESET}"
+        # Set your API keys here
+        ~/go/bin/subfinder --set-config VirustotalAPIKey=<API-KEY-HERE>
+        ~/go/bin/subfinder --set-config PassivetotalUsername=<API-KEY-HERE>,PassivetotalKey=<API-KEY-HERE>
+        ~/go/bin/subfinder --set-config SecurityTrailsKey=<API-KEY-HERE>
+        ~/go/bin/subfinder --set-config RiddlerEmail=<API-KEY-HERE>,RiddlerPassword=<API-KEY-HERE>
+        ~/go/bin/subfinder --set-config CensysUsername=<API-KEY-HERE>,CensysSecret=<API-KEY-HERE>
+        ~/go/bin/subfinder --set-config ShodanAPIKey=<API-KEY-HERE>
+    fi
+
+    installBanner "subjack"
+    if [ -e ~/go/bin/subjack ]; then
+        echo -e "${BLUE}[!] Subjack already exists...${RESET}"
+    else 
+        go get -u github.com/haccer/subjack
+    fi
+}
+
+corsTools(){
+    echo -e "${GREEN}\n--==[ Installing CORS config checker ]==--${RESET}"
+    installBanner "CORScanner"
+    if [ "$(ls -A $TOOLS_PATH/CORScanner 2>/dev/null)" ]; then
+        echo -e "${BLUE}[!] CORScanner already exists...\n${RESET}"
+    else
+        cd $TOOLS_PATH
+        git clone https://github.com/chenjj/CORScanner.git
+        cd CORScanner
+        sudo pip install -r requirements.txt
+        cd $WORKING_DIR
+    fi
+}
+
+
+ipEnumTools(){ 
+    echo -e "${GREEN}\n--==[ Installing IP enum tools ]==--${RESET}"
+    installBanner "massdns"
+    if [ -e $TOOLS_PATH/massdns/bin/massdns 2>/dev/null ]; then
+        echo -e "${BLUE}[!] Massdns already installed...\n${RESET}"
+    else
+        cd $TOOLS_PATH
+        git clone https://github.com/blechschmidt/massdns
+        cd massdns
+        make -j
+        cd $WORKING_DIR
+    fi
+}
+
+
+portScanTools(){
+    echo -e "${GREEN}\n--==[ Installing port scanners ]==--${RESET}"
+    installBanner "masscan"
+    if [ -e $TOOLS_PATH/masscan/bin/masscan 2>/dev/null ]; then
+        echo -e "${BLUE}[!] Masscan already installed...\n${RESET}"
+    else
+        cd $TOOLS_PATH
+        git clone https://github.com/robertdavidgraham/masscan
+        cd masscan
+        make -j
+        cd $WORKING_DIR
+    fi
+
+    LATEST_NMAP="$(wget -qO- https://nmap.org/dist/ | grep -oP 'nmap-([0-9\.]+)\.tar\.bz2'| tail -n 1 | grep -oP 'nmap-[0-9\.]+' | grep -oP '[0-9\.]+' | head -c -2)"
+    if [ ! -x "$(command -v nmap)" ]; then
+        installBanner "nmap"
+        wget https://nmap.org/dist/nmap-$LATEST_NMAP.tar.bz2
+        bzip2 -cd nmap-$LATEST_NMAP.tar.bz2 | tar xvf -
+        cd nmap-$LATEST_NMAP
+        ./configure
+        make -j
+        sudo make -j install
+        cd $WORKING_DIR
+        rm -rf nmap-$LATEST_NMAP*
+    else 
+        if [ "$LATEST_NMAP" == "$(nmap -V | grep version | cut -d " " -f 3)" ]; then
+            echo -e "${BLUE}[!] Latest version of Nmap already installed...${RESET}"
+        else
+            echo -e "${BLUE}[!] Upgrading to the latest version of Nmap...${RESET}"
+            wget https://nmap.org/dist/nmap-$LATEST_NMAP.tar.bz2
+            bzip2 -cd nmap-$LATEST_NMAP.tar.bz2 | tar xvf -
+            cd nmap-$LATEST_NMAP
+            ./configure
+            make -j
+            sudo make -j install
+            cd $WORKING_DIR
+            rm -rf nmap-$LATEST_NMAP*
+        fi 
+    fi
+}
+
+
+visualReconTools(){
+    echo -e "${GREEN}\n--==[ Installing visual recon tools ]==--${RESET}"
+    installBanner "aquatone"
+    if [ -e ~/go/bin/aquatone ]; then
+        echo -e "${BLUE}[!] Aquatone already exists...\n${RESET}"
+    else 
+        go get -u github.com/michenriksen/aquatone
+    fi
+}
+
+
+dirBruteTools(){
+    echo -e "${GREEN}\n--==[ Installing content discovery tools ]==--${RESET}"
+    installBanner "dirsearch"
+    if [ "$(ls -A $TOOLS_PATH/dirsearch 2>/dev/null)" ]; then
+        echo -e "${BLUE}[!] Dirsearch already exists...\n${RESET}"
+    else
+        cd $TOOLS_PATH
+        git clone https://github.com/maurosoria/dirsearch
+        cd $WORKING_DIR
+    fi
+}
+
+
+otherTools(){
+    echo -e "${GREEN}\n--==[ Downloading wordlists & other tools]==--${RESET}"
+    if [ -e $WORDLIST_PATH/dns_all.txt 2>/dev/null ] && [ -e $WORDLIST_PATH/raft-large-words.txt 2>/dev/null ]; then
+        echo -e "${BLUE}[!] Wordlists already downloaded...\n${RESET}"
+    else
+        echo -e "${RED}[+] Downloading wordlists...${RESET}"
+        wget -O $WORDLIST_PATH/dns_all.txt https://gist.githubusercontent.com/jhaddix/86a06c5dc309d08580a018c66354a056/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt
+        wget -O $WORDLIST_PATH/raft-large-words.txt https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/raft-large-words.txt
+    fi
+
+    if [ -e $TOOLS_PATH/nmap-bootstrap.xsl 2>/dev/null ]; then
+        echo -e "${BLUE}[!] Nmap-bootstrap.xsl already downloaded...\n${RESET}"
+    else
+        echo -e "${RED}[+] Downloading nmap-bootstrap-xsl...${RESET}"
+        wget -O $TOOLS_PATH/nmap-bootstrap.xsl https://github.com/honze-net/nmap-bootstrap-xsl/raw/master/nmap-bootstrap.xsl
+    fi
+}
+
+
+# Main function
+update
+createDir
+setupTools
+subEnumTools
+corsTools
+ipEnumTools
+portScanTools
+visualReconTools
+dirBruteTools
+otherTools
+
+echo -e "${GREEN}--==[ DONE ]==--${RESET}"
