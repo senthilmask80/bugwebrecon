549b7eee6f9c01f4e8a8b4eb1b50b32e0f11f0a1
senthilmask80 <53327340+senthilmask80@users.noreply.github.com>
2023-01-18 11:54:08 +0530

    Update and rename prog-tools.sh to prog-tools-linux-amd64.sh

---
+++ prog-tools-linux-amd64.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# https://github.com/tomnomnom?tab=repositories
+# Pending fff, gf, 
+wget https://github.com/tomnomnom/assetfinder/releases/download/v0.1.1/assetfinder-linux-amd64-0.1.1.tgz
+wget https://github.com/tomnomnom/unfurl/releases/download/v0.4.3/unfurl-linux-amd64-0.4.3.tgz
+wget 
+
+
+
+go get -u github.com/tomnomnom/gf;
+go install github.com/tomnomnom/gf@latest;
+go get -u github.com/tomnomnom/fff;
+go install github.com/tomnomnom/fff@latest;
--- prog-tools.sh
+++
@@ -1,5 +0,0 @@
-!#/bin/bash
-
-# https://github.com/tomnomnom?tab=repositories
-wget https://github.com/tomnomnom/assetfinder/releases/download/v0.1.1/assetfinder-linux-amd64-0.1.1.tgz
-wget 
