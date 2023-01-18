26ea717b25b51f6fb017f55870dfc90ba52ec32a
senthilmask80 <53327340+senthilmask80@users.noreply.github.com>
2023-01-18 18:09:13 +0530

    Update prog-tools-linux-amd64.sh

--- prog-tools-linux-amd64.sh
+++ prog-tools-linux-amd64.sh
@@ -185,6 +185,7 @@ git clone https://github.com/codingo/Interlace.git
 git clone https://github.com/devanshbatham/OpenRedireX.git
 git clone https://github.com/devanshbatham/ParamSpider.git
 git clone https://github.com/SpiderLabs/Responder.git
+git clone https://github.com/sa7mon/S3Scanner.git
 
 
 
@@ -203,10 +204,13 @@ wget https://github.com/wappalyzer/wappalyzer/releases/download/v6.10.54/webexte
 wget https://github.com/wappalyzer/wappalyzer/releases/download/v6.10.54/webextension-v3.zip;
 wget https://github.com/xm1k3/cent/releases/download/v1.0/cent;
 wget https://github.com/rs/dnstrace/releases/download/v1.4.0/dnstrace_1.4.0_linux_amd64.deb;
+wget https://github.com/RustScan/RustScan/releases/download/1.10.0/rustscan_1.10.0_amd64.deb;
+wget https://github.com/nccgroup/scrying/releases/download/v0.9.2/scrying_0.9.2_amd64.deb;
 git clone --depth=1 https://github.com/jaeles-project/jaeles-signatures /tmp/jaeles-signatures/
 git clone https://github.com/jaeles-project/jaeles-plugins.git
 git clone https://github.com/six2dez/burp-bounty-profiles.git
 git clone https://github.com/wagiro/BurpBounty/releases/download/BurpBounty_v4.0/BurpBountyFree_v4.0.jar
+
 
 
 
