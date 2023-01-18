2268d520943a207438369561d8627c60088390ac
senthilmask80 <53327340+senthilmask80@users.noreply.github.com>
2023-01-19 00:26:16 +0530

    Update recon-tools.sh

--- recon-tools.sh
+++ recon-tools.sh
@@ -1,8 +1,12 @@
-#! /bin/bash
+#!/bin/bash
 
 git clone https://github.com/tdr130/assetnote.git;
+
 wget https://github.com/laramies/theHarvester/archive/refs/tags/4.2.0.zip;
 wget https://github.com/six2dez/reconftw/archive/refs/tags/v2.5.1.zip;
 wget https://github.com/six2dez/resolvers_reconftw.git
 wget https://github.com/pry0cc/axiom/archive/refs/tags/0.34.zip
 wget https://github.com/kh4sh3i/smartrecon/archive/refs/tags/v1.4.0.zip
+
+#Docker Based Tools
+git clone https://github.com/SabyasachiRana/WebMap.git
