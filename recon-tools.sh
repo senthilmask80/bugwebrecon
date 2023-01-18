3f042d8963a7698fd6e0f91783c0139543336a0b
senthilmask80 <53327340+senthilmask80@users.noreply.github.com>
2023-01-18 17:29:26 +0530

    Update recon-tools.sh

--- recon-tools.sh
+++ recon-tools.sh
@@ -5,4 +5,4 @@ wget https://github.com/laramies/theHarvester/archive/refs/tags/4.2.0.zip;
 wget https://github.com/six2dez/reconftw/archive/refs/tags/v2.5.1.zip;
 wget https://github.com/six2dez/resolvers_reconftw.git
 wget https://github.com/pry0cc/axiom/archive/refs/tags/0.34.zip
-
+wget https://github.com/kh4sh3i/smartrecon/archive/refs/tags/v1.4.0.zip
