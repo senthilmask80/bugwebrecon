2081d205a9cc0642b8b213a7a4f39cb9c9aabd6c
senthilmask80 <53327340+senthilmask80@users.noreply.github.com>
2023-01-18 18:45:01 +0530

    Update buginstall.sh

--- buginstall.sh
+++ buginstall.sh
@@ -563,3 +563,8 @@ install nmap
 install silversearcher-ag
 
 
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
+  chmod 755 msfinstall && \
+  ./msfinstall
+  
+
