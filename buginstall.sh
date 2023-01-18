bcf0f46e207854132dedbb9180e10c7d9bcbd1b6
senthilmask80 <53327340+senthilmask80@users.noreply.github.com>
2023-01-18 18:18:35 +0530

    Update buginstall.sh

--- buginstall.sh
+++ buginstall.sh
@@ -60,6 +60,12 @@ sudo apt-get install onionshare onioncircuits onionshare unbound proxychains pro
 sudo apt-get install chromium-browser chromium-chromedriver libnet-ssleay-perl; 
 sudo apt-get install masscan whatweb sublist3r gobuster nikto wafw00f medusa
 sudo apt-get install 0install 0install-core;
+apt-get install libssl-dev libssh-dev libidn11-dev libpcre3-dev \
+                 libgtk2.0-dev libmysqlclient-dev libpq-dev libsvn-dev \
+                 firebird-dev libmemcached-dev libgpg-error-dev \
+                 libgcrypt11-dev libgcrypt20-dev
+
+
 pip install pipreqs;
 pip install pypi-install;
 pip install feedparser;
