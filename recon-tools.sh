b3b87d50e6a5d3a920afd83a721c59846c720922
senthilmask80 <53327340+senthilmask80@users.noreply.github.com>
2023-01-19 01:26:39 +0530

    Update recon-tools.sh

--- recon-tools.sh
+++ recon-tools.sh
@@ -13,3 +13,6 @@ wget https://github.com/kh4sh3i/smartrecon/archive/refs/tags/v1.4.0.zip
 #Docker Based Tools
 git clone https://github.com/SabyasachiRana/WebMap.git
 git clone https://github.com/yogeshojha/rengine.git
+git clone https://github.com/intrigueio/intrigue-core.git
+
+
