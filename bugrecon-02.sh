88e079fa5c36bb585be6eba5e2e6884482014135
Senthil Kumaran <53327340+senthilmask80@users.noreply.github.com>
2023-03-09 19:03:30 +0530

    Create bugrecon-02.sh

---
+++ bugrecon-02.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+
+echo -e "\n-----------------------------------------"
+echo -e "${BOLD}${LIGHT_YELLOW}[~] Installing python tools${NORMAL}"
+echo "-----------------------------------------"
+
+STARTIME=$(date +%s)
+LOGFILE="install.log"
+
+BLINK='\e[5m'
+BOLD='\e[1m'
+LIGHT_GREEN='\e[92m'
+LIGHT_YELLOW='\e[93m'
+LIGHT_CYAN='\e[96m'
+NORMAL='\e[0m'
+RED='\e[31m'
+UNDERLINE='\e[4m'
+
+if ! testcmd amass; then
+    export GO111MODULE=on
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing amass...${NORMAL}"
+    go get -u github.com/OWASP/Amass/v3/...
+else
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing amass...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
+fi
+
+if ! testcmd subfinder; then
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing subfinder...${NORMAL}"
+    go get github.com/projectdiscovery/subfinder/cmd/subfinder
+else
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing subfinder...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
+fi
+
+if ! testcmd gobuster; then
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing gobuster...${NORMAL}"
+    go get github.com/OJ/gobuster
+else
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing gobuster...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
+fi
+
+if ! testcmd waybackurls; then
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing waybackurls...${NORMAL}"
+    go get -u github.com/tomnomnom/waybackurls
+else
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing waybackurls...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
+fi
+
+if ! testcmd waybackunifier; then
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing waybackunifier...${NORMAL}"
+    go get github.com/mhmdiaa/waybackunifier
+else
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing waybackunifier...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
+fi
+
+if ! testcmd fff; then
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing fff...${NORMAL}"
+    go get -u github.com/tomnomnom/hacks/fff
+else
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing fff...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
+fi
+
+if ! testcmd httprobe; then
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing httprobe...${NORMAL}"
+    go get -u github.com/tomnomnom/httprobe
+else
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing httprobe...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
+fi
+
+if ! testcmd meg; then
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing meg...${NORMAL}"
+    go get -u github.com/tomnomnom/meg
+else
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing meg...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
+fi
+
+if ! testcmd unfurl; then
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing unfurl...${NORMAL}"
+    go get -u github.com/tomnomnom/unfurl
+else
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing unfurl...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
+fi
+
+if ! testcmd filter-resolved; then
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing filter-resolved...${NORMAL}"
+    go get -u github.com/tomnomnom/hacks/filter-resolved
+else
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing filter-resolved...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
+fi
+
+if ! testcmd gowitness; then
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing gowitness...${NORMAL}"
+    go get -u github.com/sensepost/gowitness
+else
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing gowitness...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
+fi
+
+if ! testcmd getJS; then
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing GetJS...${NORMAL}"
+    go get -u github.com/003random/getJS
+else
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing GetJS...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
+fi
+
+if ! testcmd subzy; then
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing Subzy...${NORMAL}"
+    go get -u github.com/lukasikic/subzy
+else
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing Subzy...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
+fi
+
+if ! testcmd SubOver; then
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing SubOver...${NORMAL}"
+    go get -u github.com/Ice3man543/SubOver
+else
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing SubOver...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
+fi
+
+if ! testcmd shhgit; then
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing Shhgit...${NORMAL}"
+    go get github.com/eth0izzle/shhgit
+else
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing Shhgit...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
+fi
+
+if ! testcmd gitrob; then
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing gitrob...${NORMAL}"
+    go get github.com/michenriksen/gitrob
+else
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing gitrob...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
+fi
+
+if ! testcmd ffuf; then
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing ffuf...${NORMAL}"
+    go get github.com/ffuf/ffuf
+else
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing ffuf...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
+fi
+
+if ! testcmd unisub; then
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing unisub...${NORMAL}"
+    go get -u github.com/tomnomnom/hacks/unisub
+else
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing unisub...${LIGHT_YELLOW}[ALREADY INSTALLED]${NORMAL}"
+fi
+
+if ! testcmd aquatone; then
+    echo -e "${BOLD}${LIGHT_GREEN}[+] Installing aquatone...${NORMAL}"
+    AQUATONE="aquatone-1.7.0.zip"
+    wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip -O $AQUATONE
+    unzip $AQUATONE -x LICENSE.txt -x README.md
+    sudo mv aquatone /usr/local/bin
+    rm -rf $AQUATONE
+fi
