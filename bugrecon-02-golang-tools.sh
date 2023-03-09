8c5bbf799308a97afb59036c2a776a7427a9dcf6
Senthil Kumaran <53327340+senthilmask80@users.noreply.github.com>
2023-03-09 19:13:42 +0530

    Update bugrecon-02-golang-tools.sh

--- bugrecon-02-golang-tools.sh
+++ bugrecon-02-golang-tools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "\n-----------------------------------------"
-echo -e "${BOLD}${LIGHT_YELLOW}[~] Installing python tools${NORMAL}"
+echo -e "${BOLD}${LIGHT_YELLOW}[~] Installing golang tools${NORMAL}"
 echo "-----------------------------------------"
 
 STARTIME=$(date +%s)
